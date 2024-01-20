4c38f7edd26b65a912e10ffd59430b65cd4d3e69
PacifistRam <141394720+PacifistRam@users.noreply.github.com>
2024-01-20 21:39:30 +0530

    added API integration

---
+++ script.js
@@ -0,0 +1,80 @@
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.querySelector('#search-input');
+const searchButton = document.querySelector('#search-button');
+
+const locationName = document.querySelector('.location-name');
+const currentDateValue = document.querySelector('.date');
+const currentDay = document.querySelector('.day');
+const locationTime = document.querySelector('.location-time');
+const WeatherImage = document.querySelector('#weather-image');
+const WeatherDescription = document.querySelector('#weather-description');
+const currentTemperature = document.querySelector('.temperature');
+
+const daysOfWeek = ['Sunday','Monday','Tuesday','wednesday','Thursday','Friday','Saturday'] 
+
+
+const display = (e) => {
+    e.preventDefault();
+    const searchValue = searchInput.value.trim().replace(/\s+/g, ' ');
+    if (searchValue) {
+        // errorMessage.textContent = ''
+        weatherData(searchValue);
+        searchInput.value = ''
+    } else {
+        console.log('search is empty')
+        // errorMessage.textContent = 'Please enter a city name'
+    }
+}
+
+searchForm.addEventListener('submit', display);
+
+  async function weatherData(locationName) {
+    try {
+      const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=b1f65c73a4144e70ad460955241301&q=${locationName}&days=3`);
+      const weatherData = await response.json();
+      // console.log(weatherData.location);
+      const{location,current,forecast} = weatherData
+      const[currentDate,localTime] = location.localtime.split(" ")
+      
+      if(location && current && forecast){
+        // console.log(location)
+        console.log(current)
+        // console.log(forecast)
+        console.log(currentDate);
+        console.log(localTime);
+        renderLocationData(location,currentDate,localTime);
+        renderCurrentWeatherData(current);
+      }
+    
+    }
+    catch (error) {
+    //   errorMessage.textContent = "can't locate the place you entered"
+      console.error('Error fetching data:', error);
+    }
+}
+
+const renderLocationData= (location,currentDate,localTime) =>{
+  locationName.textContent =`${location.name},${location.region}`
+  localDayAndDate(currentDate,localTime)
+}
+
+const localDayAndDate = (currentDate,localTime) =>{
+  const localTimeAsLocal = new Date(localTime);
+  console.log(localTimeAsLocal);
+  locationTime.textContent = localTime;
+
+  const dayDate = new Date(currentDate);
+  const dayIndex = dayDate.getDay();
+  const dayName = daysOfWeek[dayIndex];
+
+  currentDateValue.textContent = dayDate.toLocaleDateString();
+  currentDay.textContent = dayName;
+ 
+}
+
+const renderCurrentWeatherData = (current) =>{
+  const tempInCelsius  =current.temp_c
+  const tempInFahrenheit  =current.temp_f
+  currentTemperature.textContent  =`${tempInCelsius}°C`
+}
+weatherData("panaji");
